57f3326069ce35bd9b52137a69d85332e2fe5233
David Multiverze Washington <multimediaspecialist@outlook.com>
2024-07-26 15:57:44 -0700

    Add files via upload

---
+++ ArtificialIntellegence.js
@@ -0,0 +1,199 @@
+<div id="character1" onclick="select();" onmouseover=""></div>
+
+<div id="character2" onclick="select();" onscroll=""></div>
+
+<div id="character3" onclick="select();" onbuttonpress="" ></div>
+
+<div id="character4" onclick="select();" ></div>
+
+<div id="character5" onclick="select();"></div>
+
+<div id="character6" onclick="select();"></div>
+
+<div id="character7" onclick="select();"></div>
+
+<div id="character8" onclick="select();"></div>
+
+<div id="character9" onclick="select();"></div>
+
+<div id="character10" onclick="select();"></div>
+
+<div id="character11" onclick="select();"></div>
+
+<div id="character12" onclick="select();"></div>
+
+<div id="character13" onclick="select();"></div>
+
+<div id="character14" onclick="select();"></div>
+
+<div id="character15" onclick="select();"></div>
+
+<div id="character16" onclick="select();"></div>
+
+<div id="character17" onclick="select();"></div>
+
+<div id="character18" onclick="select();"></div>
+
+<div id="character19" onclick="select();"></div>
+
+<div id="character20" onclick="select();"></div>
+
+<div id="character21" onclick="select();"></div>
+
+<div id="character22" onclick="select();"></div>
+
+<div id="character23" onclick="select();"></div>
+
+<div id="character24" onclick="select();"></div>
+
+<div id="character25" onclick="select();"></div>
+
+<div id="character26" onclick="select();"></div>
+
+<div id="character27" onclick="select();"></div>
+
+<div id="character28" onclick="select();"></div>
+
+<div id="character29" onclick="select();"></div>
+
+<div id="character30" onclick="select();"></div>
+
+<div id="character31" onclick="select();"></div>
+
+<div id="character32" onclick="select();"></div>
+
+<div id="character33" onclick="select();"></div>
+
+<div id="character34" onclick="select();"></div>
+
+<div id="character35" onclick="select();"></div>
+
+<div id="character36" onclick="select();"></div>
+
+<div id="character37" onclick="select();"></div>
+
+<div id="character38" onclick="select();"></div>
+
+<div id="character39" onclick="select();"></div>
+
+<div id="character40" onclick="select();"></div>
+
+<div id="character41" onclick="select();"></div>
+
+<div id="character42" onclick="select();"></div>
+
+<div id="character43" onclick="select();"></div>
+
+<div id="character44" onclick="select();"></div>
+
+<div id="character45" onclick="select();"></div>
+
+<div id="character46" onclick="select();"></div>
+
+<div id="character47" onclick="select();"></div>
+
+<div id="character48" onclick="select();"></div>
+
+<div id="character49" onclick="select();"></div>
+
+<div id="character50" onclick="select();"></div>
+
+<div id="character51" onclick="select();"></div>
+
+<div id="character52" onclick="select();"></div>
+
+<div id="character53" onclick="select();"></div>
+
+<div id="character54" onclick="select();"></div>
+
+<div id="character55" onclick="select();"></div>
+
+<div id="character56" onclick="select();"></div>
+
+<div id="character57" onclick="select();"></div>
+
+<div id="character58" onclick="select();"></div>
+
+<div id="character59" onclick="select();"></div>
+
+<div id="character60" onclick="select();"></div>
+
+<div id="character61" onclick="select();"></div>
+
+<div id="character62" onclick="select();"></div>
+
+<div id="character63" onclick="select();"></div>
+
+<div id="character64" onclick="select();"></div>
+
+<div id="character65" onclick="select();"></div>
+
+<div id="character66" onclick="select();"></div>
+
+<div id="character67" onclick="select();"></div>
+
+<div id="character68" onclick="select();"></div>
+
+<div id="character69" onclick="select();"></div>
+
+<div id="character70" onclick="select();"></div>
+
+<div id="character71" onclick="select();"></div>
+
+<div id="character72" onclick="select();"></div>
+
+<div id="character73" onclick="select();"></div>
+
+<div id="character74" onclick="select();"></div>
+
+<div id="character75" onclick="select();"></div>
+
+<div id="character76" onclick="select();"></div>
+
+<div id="character77" onclick="select();"></div>
+
+<div id="character78" onclick="select();"></div>
+
+<div id="character79" onclick="select();"></div>
+
+<div id="character80" onclick="select();"></div>
+
+<div id="character81" onclick="select();"></div>
+
+<div id="character82" onclick="select();"></div>
+
+<div id="character83" onclick="select();"></div>
+
+<div id="character84" onclick="select();"></div>
+
+<div id="character85" onclick="select();"></div>
+
+<div id="character86" onclick="select();"></div>
+
+<div id="character87" onclick="select();"></div>
+
+<div id="character88" onclick="select();"></div>
+
+<div id="character89" onclick="select();"></div>
+
+<div id="character90" onclick="select();"></div>
+
+<div id="character91" onclick="select();"></div>
+
+<div id="character92" onclick="select();"></div>
+
+<div id="character93" onclick="select();"></div>
+
+<div id="character94" onclick="select();"></div>
+
+<div id="character95" onclick="select();"></div>
+
+<div id="character96" onclick="select();"></div>
+
+<div id="character97" onclick="select();"></div>
+
+<div id="character98" onclick="select();"></div>
+
+<div id="character99" onclick="select();"></div>
+
+<div id="character100" onclick="select();"></div>
